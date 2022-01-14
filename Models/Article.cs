f8b84a2e1ffe056086c538565a0c5128d2b50aa4
Ryan-Bui <rb222rx@student.lnu.se>
2022-01-14 23:29:37 +0100

    try out .net

---
+++ Models/Article.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace articles_app.Models
+{
+    public class Article
+    {
+        public string ImageUrl { get; set; }
+        public string Header { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace articles_app.Models
+{
+    public class User
+    {
+        public string username { get; set; }
+    }
+}
