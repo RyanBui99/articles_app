c133ad13e2c3a1c8addef4ef3f56908dc5549375
Ryan Bui <rb222rx@student.lnu.se>
2022-01-19 14:47:52 +0100

    update and delete user BACKEND

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -40,6 +40,37 @@ namespace articles_app.Controllers
             return Ok(RemoveLoggedInAdminFromList(list));
         }
 
+        [HttpPut]
+        [Route("updateUser/{id}")]
+        public async Task<IActionResult> EditUser([FromBody] Users input, [FromRoute] string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var role = await _userManager.GetRolesAsync(user);
+            await _userManager.RemoveFromRoleAsync(user, role.First()); // Temp delete role from user
+            if (user == null)
+            {
+                return BadRequest(new ResponseToClient { Message = "User does not exist" });
+            } else
+            {
+                user.Email = input.Username;
+                user.UserName = input.Username;
+                await _userManager.UpdateAsync(user);
+                await _userManager.AddToRoleAsync(user, input.Role);
+                return Ok(new ResponseToClient { Message = "User successfully updated"});
+            }
+        }
+
+        [HttpDelete]
+        [Route("deleteUser/{id}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return BadRequest(new ResponseToClient { Message = "User does not exist" });
+
+            await _userManager.DeleteAsync(user);
+            return Ok(new ResponseToClient { Message = "User successfully deleted" });
+        }
+
         private List<Users> RemoveLoggedInAdminFromList(List<Users> UserList)
         {
             var originalAdminId = "a18be9c0-aa65-4af8-bd17-00bd9344e575";
