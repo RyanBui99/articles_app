39139f5961f15f4ee5d3642d0749954e21728dcf
Ryan Bui <rb222rx@student.lnu.se>
2022-01-26 20:49:36 +0100

    changes from server were not sent

--- Models/LoginRequestModel.cs
+++ Models/LoginRequestModel.cs
@@ -9,7 +9,7 @@ namespace articles_app.Models
     public class LoginRequestModel
     {
         [Required]
-        public string Email { get; set; }
+        public string Username { get; set; }
 
         [Required]
         public string Password { get; set; }
